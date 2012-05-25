b96c6e653221d14aa8c672eab9ba04bb7bef4bc4
Michael JasonSmith <mpj17@onlinegroups.net>
2012-05-25 16:28:27 +1000

    Splitting the About tab in two.
    --HG--
    rename : gs/group/about/browser/templates/about.pt => gs/group/about/browser/templates/abouttabadmin.pt
    rename : gs/group/about/browser/templates/about.pt => gs/group/about/browser/templates/abouttabintro.pt

--- gs/group/about/abouttab.py
+++ gs/group/about/abouttab.py
@@ -1,8 +1,11 @@
 # coding=utf-8
 from gs.group.home.admin import AdminTab
 
-class AboutTab(AdminTab):
+class AboutInfo(AdminTab):
     @property
     def aboutText(self):
         return getattr(self.context, 'aboutText', '')
+
+class AboutTab(AdminTab):
+    pass
 
--- gs/group/about/interfaces.py
+++ gs/group/about/interfaces.py
@@ -1,9 +1,13 @@
 # coding=utf-8
 from zope.interface.interface import Interface
 from zope.schema import Text
+from zope.viewlet.interfaces import IViewletManager
 
 class IChangeAbout(Interface):
     aboutText = Text(title=u'Text',
         description=u'The text that appears in the About tab.',
         required = False)
+
+class IGroupAboutInfo(IViewletManager):
+    '''All the information in the viewlet manager'''
 
