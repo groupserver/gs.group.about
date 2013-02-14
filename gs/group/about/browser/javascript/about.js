e21efa027317faa00b0d9e5ab166b59c6859662a
Michael JasonSmith <mpj17@onlinegroups.net>
2013-02-14 13:05:15 +1100

    Following the name change of the with_module function

--- gs/group/about/browser/javascript/about.js
+++ gs/group/about/browser/javascript/about.js
@@ -1,10 +1,7 @@
 jQuery.noConflict();
-
-var setup_about = function () {
-    GSMoreWidget('#gs-group-about-tab-intro');
-}
-
 jQuery(window).load(function () {
-    gsJsLoader.with('/++resource++gs-content-js-more-20130110.js',
-                    setup_about);
+    gsJsLoader.with_module('/++resource++gs-content-js-more-20130110.js',
+                           function(){
+                               GSMoreWidget('#gs-group-about-tab-intro');
+                           });
 });
