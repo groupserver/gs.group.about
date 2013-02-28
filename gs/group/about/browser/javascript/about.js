19c5d63c6c79a463394a37a0a7b42d633e91bf72
Michael JasonSmith <mpj17@onlinegroups.net>
2013-02-28 17:01:12 +1100

    Making the JavaScript easier to read.

--- gs/group/about/browser/javascript/about.js
+++ gs/group/about/browser/javascript/about.js
@@ -1,7 +1,9 @@
 jQuery.noConflict();
+
+function gs_group_about_init () {
+    GSMoreWidget('#gs-group-about-tab-intro');
+}
+
 jQuery(window).load(function () {
     gsJsLoader.with_module('/++resource++gs-content-js-more-20130110.js',
-                           function(){
-                               GSMoreWidget('#gs-group-about-tab-intro');
-                           });
-});
+                           gs_group_about_init)});
