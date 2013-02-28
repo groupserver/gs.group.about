ffdbcb8393eec434ffe157e52154ed6d12cd3d89
Michael JasonSmith <mpj17@onlinegroups.net>
2013-02-28 17:00:28 +1100

    Minor whitespace change, and message format change, for the Change page.

--- gs/group/about/changeabout.py
+++ gs/group/about/changeabout.py
@@ -9,6 +9,7 @@ from gs.group.base.form import GroupForm
 from gs.group.home.audit import Auditor, CHANGE_ABOUT
 from interfaces import IChangeAbout
 
+
 class ChangeAbout(GroupForm):
     label = u'Change About'
     pageTemplateFileName = 'browser/templates/changeabout.pt'
@@ -16,7 +17,7 @@ class ChangeAbout(GroupForm):
 
     def __init__(self, group, request):
         GroupForm.__init__(self, group, request)
-        
+
     @Lazy
     def form_fields(self):
         enforce_schema(self.context, IChangeAbout)
@@ -27,18 +28,17 @@ class ChangeAbout(GroupForm):
     @form.action(label=u'Change', failure='handle_change_action_failure')
     def handle_invite(self, action, data):
         form.applyChanges(self.context, self.form_fields, data)
-        
+
         auditor = Auditor(self.siteInfo, self.groupInfo)
         admin = createObject('groupserver.LoggedInUser', self.context)
         auditor.info(CHANGE_ABOUT, admin, '%d' % len(data['aboutText']))
-        
-        self.status = u'The About tab on the homepage for '\
-          u'<a href="%s">%s</a> has been changed.' %\
-          (self.groupInfo.relative_url(), self.groupInfo.name)
-        
+
+        s = u'The About tab on the homepage for <a href="{0}/">{1}</a> has '\
+            u'been changed.'
+        self.status = s.format(self.groupInfo.relativeURL, self.groupInfo.name)
+
     def handle_change_action_failure(self, action, data, errors):
         if len(errors) == 1:
             self.status = u'<p>There is an error:</p>'
         else:
             self.status = u'<p>There are errors:</p>'
-
